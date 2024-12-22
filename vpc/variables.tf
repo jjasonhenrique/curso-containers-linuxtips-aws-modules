40477a2bfeca3028266d278bd92d2fe672199a27
Jason Henrique <jhmr.jason@outlook.com>
2024-12-22 14:54:45 -0300

    Aula13

---
+++ vpc/variables.tf
@@ -0,0 +1,37 @@
+variable "project_name" {
+  type        = string
+  default     = ""
+  description = "Name of project"
+}
+
+variable "region" {
+  type        = string
+  default     = ""
+  description = "Region of AWS"
+}
+
+variable "cidr" {
+  type    = string
+  default = ""
+}
+
+variable "availability_zones" {
+  type    = list(string)
+  default = ""
+}
+
+variable "private_subnets" {
+  type = list(map(object({
+    name              = string
+    cidr              = string
+    availability_zone = string
+  })))
+}
+
+variable "public_subnets" {
+  type = list(map(object({
+    name              = string
+    cidr              = string
+    availability_zone = string
+  })))
+}
